7f66b741c271bc8bdc2972e8a014a38954829f80
lxz <liuxiongzhuo@163.com>
2026-03-02 16:40:58 +0800

    完善了验证过程

--- handler/get.go
+++ handler/get.go
@@ -2,15 +2,15 @@ package handler
 
 import (
 	"net/http"
+	"regexp"
 	"small_imgbed/internal/storage"
-	"strings"
 )
 
 func Get() http.Handler {
 	router := http.NewServeMux()
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		p := r.URL.Path
-		if !strings.HasPrefix(p, "/") {
+		if !isValid(p) {
 			w.WriteHeader(400)
 			return
 		}
@@ -21,4 +21,12 @@ func Get() http.Handler {
 		w.Write(storage.Get(p[1:]))
 	})
 	return router
+}
+
+var pattern = regexp.MustCompile(
+	`^/\d{4}/\d{2}/\d{2}/[a-zA-Z0-9-]+\.[a-zA-Z0-9]+$`,
+)
+
+func isValid(s string) bool {
+	return pattern.MatchString(s)
 }
